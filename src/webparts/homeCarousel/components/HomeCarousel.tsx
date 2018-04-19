8379dc859289932009af16a566dc04a5b2660af9
Sesha Guptha Chekka <schekka@qti.qualcomm.com>
2018-04-19 23:56:26 +0530

    updated column layout

--- src/webparts/homeCarousel/components/HomeCarousel.tsx
+++ src/webparts/homeCarousel/components/HomeCarousel.tsx
@@ -17,6 +17,14 @@ export default class HomeCarousel extends React.Component<IHomeCarouselProps, {}
                 <span className={ styles.label }>Learn more</span>
               </a>
             </div>
+            <div className={ styles.column }>
+            <span className={ styles.title }>Welcome to SharePoint!</span>
+              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
+              <p className={ styles.description }>{escape(this.props.description)}</p>
+              <a href="https://aka.ms/spfx" className={ styles.button }>
+                <span className={ styles.label }>Learn more</span>
+              </a>
+            </div>
           </div>
         </div>
       </div>
